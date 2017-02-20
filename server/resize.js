37855e6f30337a9ba7e0eb7953083e489d73323e
Anne Blankert <anne.blankert@geodan.nl>
2017-02-20 21:07:51 +0100

    server: adding (smaller) preview photo's

---
+++ server/resize.js
@@ -0,0 +1,34 @@
+var gm = require('gm');
+
+var fs = require('fs');
+var Path = require('path');
+var async = require("async");
+
+function resizeImages(inputdir, outputdir)
+{
+	fs.readdir(inputdir, function (err, files) {
+		console.log('files.length: ' + files.length);
+		async.eachSeries(files, function (file, callback) {
+			if (file.substr(-4, 4) == '.jpg') {
+				var inputFilename = Path.join(inputdir, file);
+				var outputFilename = Path.join(outputdir, Path.parse(file).base);
+				gm(inputFilename).resize('200', '200', '^').write(outputFilename, function(err){
+					if (err) {
+						console.log('error resizing file: ' + outputFilename);
+						callback(null);
+					} else {
+						callback(null);
+					}
+				});
+			} else {
+				callback(null);
+			}		
+			
+		});
+	});
+}
+
+resizeImages('uploads', 'uploads/preview');
+
+
+
