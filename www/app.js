6c379ad36f5923ed0abbaa73eda2e97cfdcf3e0a
Anne Blankert <anne.blankert@geodan.nl>
2017-05-09 00:03:37 +0200

    adding carbon storage calculations

--- www/app.js
+++ www/app.js
@@ -1843,6 +1843,7 @@ var App = function() {
           callback(true, 'getTagList failed');
         } else  {
           var tagtext = '';
+          var circumference = null;
           if (tags.length > 0) {
             for (var i=0; i < tags.length; i++) {
               for (var key in tags[i]) {
@@ -1855,6 +1856,9 @@ var App = function() {
                     if ((tags[i])[key] !== '') {
                       tagtext += ': ' + (tags[i])[key];
                     }
+                    if (key == "5") {
+                        circumference = parseFloat((tags[i])[key]);
+                    }
                   }
                 }
               }
@@ -1862,6 +1866,13 @@ var App = function() {
           }
           if (tagtext !== '') {
             tagtext = _utils.escapeHTML(tagtext) + '<br>';
+          }
+          if (circumference) {
+            // calculate CO2 content based on tree circumference
+            var conifer_carbon = Math.round((Math.pow((circumference/Math.PI), 2.397)*0.111)/2);
+            var deciduous_carbon = Math.round((Math.pow((circumference/Math.PI), 2.14)*0.41354)/2);
+            tagtext += 'conifer carbon weight: ' + conifer_carbon + ' kg, deciduous carbon weight: ' + deciduous_carbon + ' kg<br>';
+            tagtext += 'phone battery charges: ' + Math.round(conifer_carbon / 0.00272) + ' | ' + Math.round(deciduous_carbon / 0.00272) + '<br>';
           }
           var date = new Date();
           date.setTime(Date.parse(time));
