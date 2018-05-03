f7bacaff991f7c9a674c40013bb2bdfd2d030ec2
Anne Blankert <anne.blankert@geodan.nl>
2018-05-03 13:16:19 +0200

    adding database reload/reset script

---
+++ server2/resetdb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo -u postgres psql -c "drop database locophoto";
+sudo -u postgres psql -c "create database locophoto";
+sudo -u postgres psql locophoto -c "create extension postgis";
+sudo -u postgres psql locophoto -f ~/locophoto.sql
+
+export PGPASSWORD=geodb
+export PGUSER=geodb
+psql -h localhost locophoto -c "create table likes (id serial, photosetid int, userid int)";
+psql -h localhost locophoto -c "create unique index likesphotosetuseridx on likes (photosetid, userid)"
+psql -h localhost locophoto -c "alter table photouser add column displayname varchar"
+psql -h localhost locophoto -c "alter table photo add column photosetid int default 0"
+psql -h localhost locophoto -c "update photo set photosetid=rootid where rootid>0"
+psql -h localhost locophoto -c "update photo p1 set photosetid=p2.rootid from photo p2 where p1.photosetid=0 and p2.rootid=p1.id and p2.rootid>0"
+
+
+
