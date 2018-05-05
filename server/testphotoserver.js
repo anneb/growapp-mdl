e4c31990fde10314b401faef86135792962917bd
Anne Blankert <anne.blankert@geodan.nl>
2018-05-05 13:02:22 +0200

    working on photoserver test

--- server/testphotoserver.js
+++ server/testphotoserver.js
@@ -16,6 +16,73 @@ if (!fs.existsSync(__dirname + '/userinfo.json')) {
 
 let deviceinfo = require(__dirname + '/userinfo.json');
 
+function sleep(ms) {
+    return new Promise(resolve=> setTimeout(resolve, ms));
+}
+
+async function insertPhoto(rootid) {
+    // send a photo
+    const sendPhotoresult = await request({
+        uri: baseUrl + '/photoserver/sendphoto',
+        method: 'POST',
+        form: {
+            username: deviceinfo.email,
+            hash: deviceinfo.hash,
+            deviceid: deviceinfo.deviceid,
+            devicehash: deviceinfo.devicehash,
+            latitude: 52.3423,
+            longitude: 4.913040,
+            accuracy: 10,
+            rootid: rootid,
+            photo: fs.readFileSync(__dirname + '/../www/trees.jpg').toString('base64')
+        }
+    });
+    return sendPhotoresult;
+}
+
+async function deletePhoto(filename) {
+    const deletePhotoResult = await request({
+        uri: baseUrl + '/photoserver/deletemyphoto',
+        method: 'POST',
+        form: {
+            deviceid: deviceinfo.deviceid,
+            devicehash: deviceinfo.devicehash,
+            filename: filename
+        }
+    });
+    return deletePhotoResult;
+}
+
+async function getLastInsertedPhoto() {
+    // retrieve all photos
+    const allPhotos = await request({
+        uri:baseUrl+'/photoserver/getphotos',
+        json: true
+    });
+    if (!allPhotos.type=='FeatureCollection') {
+        throw 'unexpected result from getphotos';
+    }
+    //get photo with highest id at test location
+    let result = null;
+    const insertedPhoto = allPhotos.features.reduce((result, feature)=>feature.geometry.coordinates[0]==4.91304&&feature.geometry.coordinates[1]==52.3423&&(result?feature.properties.id>result.properties.id:true)?feature:result);
+    if (!insertedPhoto) {
+        throw 'newly added photo not found';
+    }
+    return insertedPhoto;
+}
+
+async function getPhotoset(rootid) {
+    const result = await request({
+        uri:baseUrl + '/photoserver/getphotoset',
+        json: true,
+        method: 'POST',
+        form: {
+            photoid: rootid
+        }
+    });
+    return result;
+}
+
 async function testAll()
 {
     try {    
@@ -59,38 +126,45 @@ async function testAll()
             }            
         }
         
-        // send a photo
-        const sendPhotoresult = await request({
-            uri: baseUrl + '/photoserver/sendphoto',
-            method: 'POST',
-            form: {
-                username: deviceinfo.email,
-                hash: deviceinfo.hash,
-                deviceid: deviceinfo.deviceid,
-                devicehash: deviceinfo.devicehash,
-                latitude: 52.3423,
-                longitude: 4.913040,
-                accuracy: 10,
-                rootid: 0,
-                photo: fs.readFileSync(__dirname + '/../www/trees.jpg').toString('base64')
-            }
-        });
-        console.log(sendPhotoresult);
+        const sendPhotoresult = await insertPhoto(0);
+        const insertedPhoto = await getLastInsertedPhoto();
 
-        // retrieve all photos
-        const allPhotos = await request({
-            uri:baseUrl+'/photoserver/getphotos',
-            json: true
-        });
-        if (allPhotos.type=='FeatureCollection') {
-            console.log('Number of photo locations: ' + allPhotos.features.length);            
-        } else {
-            throw 'unexpected result from getphotos';
-        }
-        //get photo with highest id at test location
-        const insertedPhoto = allPhotos.features.reduce((result, feature)=>feature.geometry.coordinates[0]==4.91304&&feature.geometry.coordinates[1]==52.3423&&(result?feature.properties.id>result.properties.id:true)?feature:result);
+        const rootid = insertedPhoto.properties.id;
+
+        const sendPhotoresult2 = await insertPhoto(rootid);
+
+        // wait for animation to finish
+        await sleep(3000);
+        const insertedPhoto2 = await getLastInsertedPhoto();
+
+        const sendPhotoresult3 = await insertPhoto(rootid);
+        await sleep(3000);
+        const insertedPhoto3 = await getLastInsertedPhoto();
 
         console.log(JSON.stringify(insertedPhoto));
+        console.log(JSON.stringify(insertedPhoto2));
+        console.log(JSON.stringify(insertedPhoto3));
+
+        const photoset = await getPhotoset(rootid);
+        console.log(JSON.stringify(photoset));
+
+        const deletedPhoto = await deletePhoto(photoset[0].filename);
+        console.log("delete: "  + deletedPhoto);
+        await sleep(3000);
+        const lastPhoto = await getLastInsertedPhoto();
+        console.log(JSON.stringify(lastPhoto));
+
+        const deletedPhoto2 = await deletePhoto(photoset[2].filename);
+        console.log("delete: "  + deletedPhoto2);
+        await sleep(3000);
+        const lastPhoto2 = await getLastInsertedPhoto();
+        console.log(JSON.stringify(lastPhoto2));
+
+        const deletedPhoto3 = await deletePhoto(photoset[1].filename);
+        console.log("delete: "  + deletedPhoto3);
+        await sleep(3000);
+        const lastPhoto3 = await getLastInsertedPhoto();
+        console.log(JSON.stringify(lastPhoto3));
 
     } catch (error) {
         console.log(error);
