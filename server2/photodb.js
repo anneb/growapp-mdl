a6d95eef14d1d9975b7eb09f666f4332af98c6c4
Anne Blankert <anne.blankert@geodan.nl>
2018-05-08 00:36:25 +0200

    fix sloppyness

--- server2/photodb.js
+++ server2/photodb.js
@@ -283,7 +283,7 @@
                 if (result.rows[0].displayname != userinfo.displayname || result.rows[0].allowmailing != userinfo.allowmailing)
                     // update allowmailing and displayname
                     sql = 'update photouser set displayname=$1, allowmailing=$2 where email=$3';
-                    await dbPool.query(sql, [userinfo.displayname, userinfo.allowmailing]);
+                    await dbPool.query(sql, [userinfo.displayname, userinfo.allowmailing, userinfo.username]);
             } else {
                 // new user
                 sql = "insert into photouser (email,displayname,validated,validationcode,hash,retrycount,allowmailing) values ($1,$2,false,$3,'',0,$4)";
