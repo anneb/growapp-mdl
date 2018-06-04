c5778e64a7276fd6fc1476594437d2026c7624a2
Anne Blankert <anne.blankert@geodan.nl>
2018-06-04 12:19:02 +0200

    Fix for "UserSQL does not exist"

--- server2/photodb.js
+++ server2/photodb.js
@@ -303,9 +303,10 @@
             if (result.rows.length) {
                 // user already known
                 validationcode = result.rows[0].validationcode;
-                if (result.rows[0].displayname != userinfo.displayname || result.rows[0].allowmailing != userinfo.allowmailing) {                    // update allowmailing and displayname
+                if (result.rows[0].displayname != userinfo.displayname || result.rows[0].allowmailing != userinfo.allowmailing) {
+                    // update allowmailing and displayname
                     sql = 'update photouser set displayname=$1, allowmailing=$2 where email=$3';
-                    await dbPool.query(userSQL, [userinfo.displayname, userinfo.allowmailing, userinfo.username]);
+                    await dbPool.query(sql, [userinfo.displayname, userinfo.allowmailing, userinfo.username]);
                 }
             } else {
                 // new user
