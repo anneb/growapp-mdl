82ac802d32a97057357441b835224901e71acc94
Anne Blankert <anne.blankert@geodan.nl>
2019-07-14 20:52:03 +0200

    move package.json to www/lib, use lib for account and language

--- hooks/after_prepare/add_to_manifest.js
+++ hooks/after_prepare/add_to_manifest.js
@@ -2,6 +2,7 @@
 
 // Adds lines to the Android Manifest if they are not already there
 
+/* 
 const LINES_TO_ADD = [
     {
         text: ' android:installLocation="auto"',
@@ -21,3 +22,4 @@ LINES_TO_ADD.forEach(function(lineToAdd) {
 });
 
 fs.writeFileSync(MANIFEST, manifestText);
+*/
